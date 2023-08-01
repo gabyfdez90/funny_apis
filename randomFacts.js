f56934a6f1abd495cf04cecfd7a0141dd95a8b6d
Gabriela M. Fernández <galalou2010@gmail.com>
2023-08-01 12:22:11 +0200

    randomFacts api ready

---
+++ randomFacts.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+// Using fetch inside an asyncronous function
+
+async function getRandomUselessFact1() {
+  const response = await fetch(
+    `https://uselessfacts.jsph.pl/api/v2/facts/random?language=en`
+  );
+  const data = await response.json();
+  const randomFact = data.text;
+  console.log(randomFact);
+}
+
+getRandomUselessFact1();
+
+// Using axios inside an asyncronous function
+// Step 1: Install accions with 'npn install axios'
+
+async function getRandomUselessFact2() {
+  const response = await axios.get(
+    `https://uselessfacts.jsph.pl/api/v2/facts/random?language=en`
+  );
+  const randomFact = response.data.text;
+  console.log(randomFact);
+}
+
+getRandomUselessFact2();
