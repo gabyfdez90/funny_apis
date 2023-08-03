0a98ddc781fea1fca01f27c98393e41f931d56ac
Gabriela M. Fernández <galalou2010@gmail.com>
2023-08-03 13:23:15 +0200

    added bored api cnx

---
+++ bored.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+
+// Using fetch inside an asyncronous function to get one idea of activity
+async function getAnActivityIdea1() {
+  const response = await fetch(`https://www.boredapi.com/api/activity`);
+  const data = await response.json();
+
+  const activityIdea = data.activity;
+  const type = data.type;
+  const numberOfParticipants = data.participants;
+
+  console.log(`Activity idea: ${activityIdea}`);
+  console.log(`Type: ${type}`);
+  console.log(`numberOfParticipants: ${numberOfParticipants}`);
+}
+
+getAnActivityIdea1();
+
+// Using axios inside an asyncronous function to get one idea of activity
+async function getAnActivityIdea2() {
+  const response = await axios.get(`https://www.boredapi.com/api/activity`);
+
+  const activityIdea = response.data.activity;
+  const type = response.data.type;
+  const numberOfParticipants = response.data.participants;
+
+  console.log(`Activity idea: ${activityIdea}`);
+  console.log(`Type: ${type}`);
+  console.log(`numberOfParticipants: ${numberOfParticipants}`);
+}
+
+getAnActivityIdea2();
+
+//Now, I will use axios again in a function that accepts type and participants as inputs
+// That way, it should return an activity idea filtered by those criteria.
+
+async function getActivityIdeaWithParameters(type, numberOfParticipants) {
+  type = type.toLowerCase();
+
+  const response = await axios.get(
+    `https://www.boredapi.com/api/activity?type=${type}&participants=${numberOfParticipants}`
+  );
+
+  const activityIdea = response.data.activity;
+
+  console.log(`Activity idea: ${activityIdea}`);
+  console.log(`Type: ${type}`);
+  console.log(`numberOfParticipants: ${numberOfParticipants}`);
+}
+
+getActivityIdeaWithParameters("recreational", 1);
